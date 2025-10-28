1c9a29cf3dc96d9b1c740cbc72f305b227a878e1
fAemeister <sinangurcan26@hotmail.com>
2025-10-28 17:26:08 +0300

    feat: add xp manager script

---
+++ script/global/experience_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+const REQUIRED_EXP_MULTIPLIER : float = 1.2;
+const MIN_REQUIRED_EXP : float = 100;
+
+var current_level : int = 1;
+var current_exp: float = 0;
+signal onLevelUp(level: int);
+
+func addExperience(xp: float):
+	var required_exp = self.getRequiredExperienceForLevelup()
+	var new_exp = current_exp + xp;
+	if(new_exp < required_exp):
+		current_exp = new_exp;
+	else:
+		var additional_exp = new_exp - required_exp;
+		current_level += 1;
+		onLevelUp.emit(current_level)
+		current_exp += additional_exp;
+	
+func getRequiredExperienceForLevelup() -> float:
+	return MIN_REQUIRED_EXP * pow(REQUIRED_EXP_MULTIPLIER, current_level-1);
