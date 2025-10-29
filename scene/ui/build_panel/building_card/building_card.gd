61b311a5f126863c0d09c06ba8c44c4dac37f509
fAemeister <sinangurcan26@hotmail.com>
2025-10-29 03:16:37 +0300

    chore: edit powerup manager

--- scene/ui/build_panel/building_card/building_card.gd
+++ scene/ui/build_panel/building_card/building_card.gd
@@ -8,23 +8,31 @@ extends TextureButton
 @export var tower_scene: PackedScene;
 @export var tower_variant: TowerVariation;
 
+var current_cost;
+
 func _ready() -> void:
 	#texture_rect.texture = 
 	self.attack_value.text = str(tower_variant.attack_stats.attack_damage);
-	self.label.text = "{name} {cost}".format({"name": tower_variant.tower_name, "cost": tower_variant.build_cost});
+	self.current_cost = tower_variant.build_cost
+	self._update_label();
 	self.range_value.text = "Long";
 	CurrencyManager.onCoinChange.connect(_monitor_coin)
 	PowerupManager.onPowerUpAdded.connect(_monitor_powerup)
 	
 func _monitor_coin(_current_coin: int) -> void:
-	self.disabled = !CurrencyManager.canAfford(tower_variant.build_cost);
+	self.disabled = !CurrencyManager.canAfford(current_cost);
 	
+
 func _monitor_powerup(powerup: PowerUp) -> void:
 	if(powerup.type != PowerUp.POWERUP_TYPE.BUILDING_DISCOUNT):
 		return;
-	var cost = PowerupManager.get_powered_stat_by_powerup(tower_variant.build_cost, powerup);
-	self.label.text = "{name} {cost}".format({"name": tower_variant.tower_name, "cost": cost});
+	current_cost = PowerupManager.get_powered_stat_by_powerup(tower_variant.build_cost, powerup);
+	self._update_label();
 	_monitor_coin(CurrencyManager.current_coin);
 
 func _on_pressed() -> void:
-	UiSignalBus.onTriggerNewBuild.emit(tower_scene, tower_variant.build_cost)
+	UiSignalBus.onTriggerNewBuild.emit(tower_scene, current_cost)
+	
+func _update_label() -> void:
+	self.label.text = "{name} {cost}".format({"name": tower_variant.tower_name, "cost": current_cost});
+	
